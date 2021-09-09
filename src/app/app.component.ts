6aa49cdb1b284f72bc4583a14706406aa71369a4
Silvan Paul <silvan.paul123@gmail.com>
2021-09-09 23:52:09 +0200

    finished the homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'silvanpaul-portfolio';
+  title = 'Silvan Paul - App and web developer';
 }
