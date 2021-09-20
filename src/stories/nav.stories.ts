cbbea27cb0f93e0074c847c46591b269e9706052
Silvan Paul <spaul@easysystems.nl>
2021-09-20 14:30:43 +0200

    made website more responsive and added nav story
    added storybook story for the navbar and made the website more responsive/mobile friendly

---
+++ src/stories/nav.stories.ts
@@ -0,0 +1,14 @@
+import { Meta } from '@storybook/angular/types-6-0'
+
+import { NavbarComponent } from 'src/app/navbar/navbar.component'
+
+export default {
+  title: 'Components/Navigation',
+  component: NavbarComponent,
+} as Meta;
+
+export const Primary = () => ({
+  props: {
+
+  }
+});
